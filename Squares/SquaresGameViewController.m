8667ae01d40d940bf3eeaa7292ab2a1e7a4c0643
Michael Coomey <mcoomey@gmail.com>
2014-03-14 13:44:29 -0400

    added player names and start button

--- Squares/SquaresGameViewController.m
+++ Squares/SquaresGameViewController.m
@@ -16,9 +16,27 @@
 
 @interface SquaresGameViewController ()
 @property (nonatomic, strong) SquaresGame *game;
+@property (nonatomic) BOOL playingGame;
 @end
 
 @implementation SquaresGameViewController
+
+- (IBAction)startGame:(id)sender {
+    self.playingGame = YES;
+    UIButton *btn = (UIButton *) sender;
+    btn.hidden = YES;
+    [self.view endEditing:YES];
+    [self updateScores];
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    [self.view endEditing:YES];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    return YES;
+}
 
 - (SquaresGame *) game
 {
@@ -33,6 +51,11 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // wait for user to start the game before playing
+    self.playingGame = NO;
+    self.linesRemainingLabel.hidden = YES;
+    self.currentPlayerLabel.hidden = YES;
     
     // Set action for buttons added by SquaresBoardView
     
@@ -57,29 +80,62 @@
 
         }
     }
+
     [self updateScores];
     
 }
 
 - (void)hLineTapped: (id) sender
 {
+    if (self.playingGame) {
     BoardHorizontalLine *bhl = (BoardHorizontalLine *) sender;
     if ([self.game selectHorizontalLine:bhl])
         [self updateScores];
+    }
 }
 
 - (void)vLineTapped: (id) sender
 {
+    if (self.playingGame) {
     BoardVerticalLine *bvl = (BoardVerticalLine *) sender;
     if ([self.game selectVerticalLine:bvl])
         [self updateScores];
+    }
 }
 
 - (void)updateScores
 {
-    self.linesRemainingLabel.text = [NSString stringWithFormat:@"%lu Lines Remaining", (unsigned long)[self.game linesRemaining]];
-    self.player1ScoreLabel.text = [NSString stringWithFormat:@"%lu", (unsigned long)[self.game player1Score]];
-    self.player2ScoreLabel.text = [NSString stringWithFormat:@"%lu", (unsigned long)[self.game player2Score]];
+    if (self.playingGame) {
+        
+        NSString *player1Name = [[self player1Name] text];
+        NSString *player2Name = [[self player2Name] text];
+        
+        if ([player1Name length] == 0) {
+            player1Name = @"Player 1";
+        }
+        
+        if ([player2Name length] == 0) {
+            player2Name = @"Player 2";
+        }
+        
+        NSString *cpl = [[NSString alloc] init];
+        if (self.game.currentPlayer == LineStateRed) {
+            cpl = [NSString stringWithFormat:@"%@'s turn", player1Name];
+            self.currentPlayerLabel.textColor = [UIColor redColor];
+        }
+        else {
+            cpl = [NSString stringWithFormat:@"%@'s turn", player2Name];
+            self.currentPlayerLabel.textColor = [UIColor blueColor];
+        }
+        [self.currentPlayerLabel setText:cpl];
+        self.currentPlayerLabel.hidden = NO;
+
+        self.linesRemainingLabel.text = [NSString stringWithFormat:@"%lu Lines Remaining", (unsigned long)[self.game linesRemaining]];
+        self.linesRemainingLabel.hidden = NO;
+        
+        self.player1ScoreLabel.text = [NSString stringWithFormat:@"%lu", (unsigned long)[self.game player1Score]];
+        self.player2ScoreLabel.text = [NSString stringWithFormat:@"%lu", (unsigned long)[self.game player2Score]];
+    }
 }
 
 @end
